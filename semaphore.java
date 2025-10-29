8ec215fe958a63c14cccec7adc19bcb6deba7ca1
MayasAlhafiry <166315864+MayasAlhafiry@users.noreply.github.com>
2025-10-29 21:25:02 +0300

    Refactor semaphore class and add Car class

--- semaphore.java
+++ semaphore.java
@@ -1,11 +1,11 @@
-public class semaphore {
+public class Semaphore {
 
     protected int value=0; //number of available slots
 
-    public semaphore(){  // Default constructor initializes semaphore with 0
+    public Semaphore(){  // Default constructor initializes semaphore with 0
         value=0;
     }
-    public semaphore(int value){ //constructor to set the value of the semaphore
+    public Semaphore(int value){ //constructor to set the value of the semaphore
         this.value=value;
     }
     public synchronized void p(){  // Used by a thread to request access to a resource
@@ -32,3 +32,45 @@ public class semaphore {
         return value;
     }
 }
+
+
+
+// car class
+
+class Car extends Thread {
+    int id;
+    Queue<Integer> buffer;  // waiting queque
+    
+    Semaphore empty;   // number of empty pump
+    Semaphore full;    // Number of cars in pump
+    Semaphore mutex;
+             
+    Car(int id, Queue<Integer> buffer, Semaphore empty, Semaphore full, Semaphore mutex) {
+        this.id = id;
+        this.buffer = buffer;
+        this.empty = empty;
+        this.full = full;
+        this.mutex = mutex;
+    }
+
+    
+    @Override
+    public void run () {
+        if(full.getValue() > 0) {     // if there is car waiting => new car will waite so print waiting
+            System.out.println("Car " + id + " arrived and waiting");
+        } else {System.out.println("Car " + id + " arrived");}  // if no car waiting => new car just print arrived
+        
+        empty.p();   // Wait until there is a free spot in the queue
+        
+        mutex.p();  // wait if the one entering to quque
+        
+        buffer.add(id);
+        System.out.println("Car " + id + " entered the waiting queue");
+        
+        mutex.v();
+  
+        full.v();   // notify pump that there is car ready
+    }
+}
+
+
